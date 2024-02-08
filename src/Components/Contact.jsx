60321e4a0d2131b43566aa201841ba4dafaebcc8
Gal_m <gmsylty@gmail.com>
2024-02-08 13:45:26 +0200

    change contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,52 +1,102 @@
-import React from 'react';
-import { Grid, Typography, Paper, Box, useMediaQuery, useTheme } from '@mui/material';
+import React, { useState } from 'react';
+import { Grid, Typography, Paper, Box, useMediaQuery, useTheme, TextField, Button, Container } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWaze } from '@fortawesome/free-brands-svg-icons';
+import { faWaze, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
 const Contact = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: 'הגעתי אלייך דרך האתר, אני מעוניין לקבוע תור',
+  });
+
+  const openWhatsApp = () => {
+    const { name, email, message } = formData;
+    const whatsappMessage = `היי שמי ${name}, ${message}.`;
+    const encodedMessage = encodeURIComponent(whatsappMessage);
+
+    window.open(`https://wa.me/972527015654?text=${encodedMessage}`, '_blank');
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+
   return (
-    <Grid container spacing={3} marginTop={5}>
-      {/* Left side - Google Map */}
+    <Container maxWidth="lg" style={{ backgroundColor: '#E5E5E5', minHeight: '100vh' }}>
+    <Grid container spacing={3} marginTop={5} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      {/* Left side - Image */}
       <Grid item xs={12} md={6}>
-        <Paper elevation={3} style={{ height: '400px', width: '100%', position: 'relative' }}>
-          <iframe
-            title="Google Map"
-            width="100%"
-            height="100%"
-            style={{ border: 0 }}
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3364.3113756096027!2d34.920511584824574!3d32.51782978105106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d0c452637ab2f%3A0x3606b0868d47174e!2z15TXl9eo157XldefIDUsINeQ15XXqCDXoten15nXkdeQ!5e0!3m2!1siw!2sil!4v1705339986767!5m2!1siw!2sil"
-            allowFullScreen
-          ></iframe>
+        <Paper elevation={8} style={{ height: isMobile ? '300px' : '500px', width: '100%', position: 'relative', borderRadius: isMobile ? '15px' : '15px 0 15px 15px', overflow: 'hidden', boxShadow: '0 10px 20px rgba(0, 0, 0, 0.2)' }}>
+          <img
+            src="https://i.ibb.co/RgSJPyk/image.jpg"
+            alt="Left side image"
+            style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: isMobile ? '0' : '15px' }}
+          />
         </Paper>
       </Grid>
 
       {/* Right side - Content */}
-      <Grid item xs={12} md={6} style={{ padding: isMobile ? '20px 10px' : '20px' }} marginTop={5}>
-        <Typography variant="body1" className="david-libre-bold" dir="rtl" style={{ fontSize: isMobile ? 18 : 25 }}>
-          <b>
-            כתובת: החרמון 5, אור עקיבא <br />
-            טלפון: 052-701-5654 <br />
-            אימייל:{' '}
-            <a href="mailto:AtzramDaniel22@gmail.com" style={{ color: 'inherit', textDecoration: 'underline' }}>
-              AtzramDaniel22@gmail.com
-            </a>
-          </b>
-        </Typography>
-       
+      <Grid item xs={12} md={6} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Paper elevation={3} style={{ padding: isMobile ? '10px' : '20px', width: '100%', maxWidth: isMobile ? '100%' : '400px', margin: isMobile ? '10px 0' : '0 auto', backgroundColor: 'rgba(255, 255, 255, 0.9)', borderRadius: '15px' }}>
+          {/* Form for details */}
+          <form>
+            <Typography variant="h5" align="center" style={{ color: '#333', marginBottom: '20px' }}>
+              לתיאום תור 
+            </Typography>
+            <TextField
+              label="שם מלא"
+              variant="outlined"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+              required
+              style={{ marginBottom: '15px' }}
+              InputLabelProps={{ style: { color: '#333' } }}
+            />
+            <TextField
+              label="אימייל"
+              variant="outlined"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+              required
+              InputLabelProps={{ style: { color: '#333' } }}
+              InputProps={{ classes: { outlined: 'outlinedInput' } }}
+              
+            />
 
-        <Box display="flex" justifyContent={isMobile ? 'center' : 'center'} alignItems="center" mt={5}>
-          <Box mx={2} style={{ marginLeft: '30%', marginTop: '15%'}}>
-            <a href="https://embed.waze.com/iframe?zoom=16&lat=32.518071&lon=34.918732&ct=livemap" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faWaze} size={isMobile ? '3x' : '4x'} style={{ color: '#FFF' }} />
-            </a>
-          </Box>
-        </Box>
+            {/* Button to send a message on WhatsApp */}
+            <Box display="flex" justifyContent="center" alignItems="center" mt={3}>
+              <Button
+                variant="contained"
+                style={{
+                  backgroundColor: '#25d366',
+                  color: '#FFF',
+                  fontWeight: 'bold',
+                  borderRadius: '25px', // Rounded button
+                }}
+                onClick={openWhatsApp}
+                startIcon={<FontAwesomeIcon icon={faWhatsapp} style={{ marginRight: '5px' }} />}
+              >
+                שלח הודעה
+              </Button>
+            </Box>
+          </form>
+        </Paper>
       </Grid>
     </Grid>
+  </Container>
   );
 };
 
--- src/Components/TattooTreatment.jsx
+++ src/Components/TattooTreatment.jsx
@@ -6,25 +6,25 @@ const TattooTreatment = () => {
   return (
     <>
     <Grid container justifyContent="center">
-      <Grid item xs={12} sm={8} md={6} lg={4}>
+      <Grid item xs={12} sm={8} md={6} lg={4} >
       <br />
       <br />
-      <Typography variant="body1" className="david-libre-medium" style={{ textAlign: 'right' }}>
+      <Typography variant="body1" className=".david-libre-medium" style={{ textAlign: 'right' }}>
         להוריד את הניילון/פד לאחר שעתיים מסיום הקעקוע ואז לשטוף עם מים קרים/פושרים וסבון 
         <br />
         <br />
       </Typography>
-      <Typography variant="body1" className="david-libre-medium" style={{ textAlign: 'right' }}>
+      <Typography variant="body1" className=".david-libre-medium" style={{ textAlign: 'right' }}>
         יום לאחר הקעקוע למרוח שכבה דקה של בפנטן פלוס. שלוש פעמים ביום במשך שבועיים 
         <br />
         <br />
       </Typography>
-      <Typography variant="body1" className="david-libre-medium" style={{ textAlign: 'right' }}>
+      <Typography variant="body1" className=".david-libre-medium" style={{ textAlign: 'right' }}>
         יש להמנע מחשיפה לשמש או מכניסה לים/בריכה/ג’קוזי/סאונה למשך שבועיים-שלושה 
         <br />
         <br />
       </Typography>
-      <Typography variant="body1" className="david-libre-medium" style={{ textAlign: 'right' }}>
+      <Typography variant="body1" className=".david-libre-medium" style={{ textAlign: 'right' }}>
         עדיף להימנע ככל האפשר מהזעה באזור הקעקוע במשך שבוע מתחילת הקעקוע 
         <br />
         <br />
